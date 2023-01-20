a0fd10d1f4becc4f537ed2904c98e5651883ed90
Abderrazaq O <abderrazaqoussimbarek@gmail.com>
2023-01-20 16:03:48 +0100

    1

---
+++ src/components/ClientSection/index.js
@@ -0,0 +1,16 @@
+export default function index() {
+  return (
+    <section>
+      <div>
+        <h1>Our Client</h1>
+        <p>Several selected clients, who already believe in our service.</p>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
+  )
+}
